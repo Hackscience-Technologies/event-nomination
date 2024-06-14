2052b9ce9fa042a52d6bdb99e88d2e84c0c4216e
K <kelvinoduroaboagye12@gmail.com>
2024-06-14 06:14:12 +0000

    update

--- src/config.js
+++ src/config.js
@@ -1,20 +1,20 @@
 
-export const EVENT_NAME = "UENR NUGS EXCELLENCE AWARDS";
+export const EVENT_NAME = "WOCOM EXCELLENCE AWARDS";
 
-export const EVENT_IMAGE = "https://uenrnugs.hstonline.tech/img/2024/logo1.png"
+export const EVENT_IMAGE = "https://uenrwocom.hstonline.tech/img/2024/logo1.png"
 
 export const BASE_URL = "https://hstonline.tech/hstsms"
 
 // const BASE_URL = "http://127.0.0.1:8000"
 
-export const PROGRAM_ID = "e1822d93-27f7-42d2-ba40-f3c4c11adefe"
+export const PROGRAM_ID = "4f6b0461-e46b-4593-8897-6c3f0156ae6c"
 
-export const WHATSAPP_URL = "https://chat.whatsapp.com/J3br7DnKML004xlrf5MxJB"
+export const WHATSAPP_URL = "https://chat.whatsapp.com/DT3uTP63emV4g4YM5S6jZr"
 
-export const META_DESCRIPTION = "UENR NUGS EXCELLENCE AWARDS NOMINATION"
+export const META_DESCRIPTION = "UENR WOCOM EXCELLENCE AWARDS NOMINATION"
 
-export const TITLE = "UENR NUGS EXCELLENCE AWARDS NOMINATION";
+export const TITLE = "UENR WOCOM EXCELLENCE AWARDS NOMINATION";
 
-export const ICON_URL = "https://uenrnugs.hstonline.tech/img/2024/logo1.png"; // Path to your icon
+export const ICON_URL = "https://uenrwocom.hstonline.tech/img/2024/logo1.png"; // Path to your icon
 
 
